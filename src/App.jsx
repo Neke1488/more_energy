73ecaf4c0a0be7f31912227b6eadd6d5cc595f04
Neke1488 <neket32167@yandex.ru>
2023-05-03 13:00:50 +0300

    sidebar_ready_2

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,13 @@ import AuthRootComponent from "./components";
 import Sidebar from './components/sidebar/sidebar';
 import Coaches from './pages/coaches';
 import Clients from './pages/clients';
+import Exercises from './pages/exercises';
+import Recipes from './pages/recipes';
+import Articles from './pages/articles';
+import Feel_best from './pages/feel_best';
+import Eating_level from './pages/eating_level';
+import Stickers from './pages/stickers';
+import Sample_menus from './pages/sample_menus';
 
 
 
@@ -23,6 +30,13 @@ function App() {
           <Routes>
               <Route path="clients" element={<Clients />}></Route>
               <Route path="coaches" element={<Coaches />}></Route>
+              <Route path="exercises" element={<Exercises />}></Route>
+              <Route path="recipes" element={<Recipes />}></Route>
+              <Route path="articles" element={<Articles />}></Route>
+              <Route path="sample_menus" element={<Sample_menus />}></Route>
+              <Route path="feel_best" element={<Feel_best />}></Route>
+              <Route path="eating_level" element={<Eating_level />}></Route>
+              <Route path="stickers" element={<Stickers />}></Route>
           </Routes>
           </Sidebar>
           
